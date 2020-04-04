4bb4410cc71b1a620b66dd40148e3218d952f499
Pantazis Kontakis <raybeez@gmail.com>
2020-04-04 15:16:47 +0300

    Rm ct

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -21,9 +21,9 @@ namespace API.Controllers
 
         [HttpGet]
 
-        public async Task<ActionResult<List<Activity>>> List(CancellationToken ct)
+        public async Task<ActionResult<List<Activity>>> List()
         {
-            return await _mediator.Send(new List.Query(), ct);
+            return await _mediator.Send(new List.Query());
         }
         [HttpGet("{id}")]
 
