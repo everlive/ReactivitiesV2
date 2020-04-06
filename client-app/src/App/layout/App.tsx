bddfa3cb855fd7bb8bdb6b61b1b91c4e5155f0c4
Pantazis Kontakis <raybeez@gmail.com>
2020-04-06 07:23:52 +0300

    adding navbar and assets

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -1,42 +1,32 @@
-import React, { Component } from "react";
-import { Header, Icon } from "semantic-ui-react";
+import React, { useState, useEffect, Fragment } from "react";
+import { Header, Icon, Container } from "semantic-ui-react";
 import { List } from "semantic-ui-react";
 import axios from "axios";
 import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
 
-interface IState {
-  activities: IActivity[];
-}
-
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: []
-  };
-
-  componentDidMount() {
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  useEffect(() => {
     axios
       .get<IActivity[]>("http://localhost:5000/api/activities")
       .then(response => {
-        this.setState({
-          activities: response.data
-        });
+        setActivities(response.data);
       });
-  }
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="users" />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
+  }, []);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
         <List>
-          {this.state.activities.map(activity => (
+          {activities.map(activity => (
             <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
-      </div>
-    );
-  }
-}
+      </Container>
+    </Fragment>
+  );
+};
 
 export default App;
